e03f00f69070ebb7206ec0d504682e6639277f42
NazmulHasan660 <nazmulhasanmahady992@gmail.com>
2025-09-02 15:10:11 +0600

    Delete employee directory

--- employee/redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] === 'employee') {
-    header("Location: employee_dashboard.php");
-    exit;
-} else {
-    header("Location: ../auth/login.php");
-    exit;
-}
